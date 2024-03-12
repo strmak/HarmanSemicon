78be834078b68c5b1466ce7b1f3ad41ae97a4cf1
Jongwoo Hwang <gojw3073@naver.com>
2024-03-12 16:58:02 +0900

    24.03.12

---
+++ test/test_24.03.08.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// 깃허브에서 수정됨.
+void main() 
+{
+	// Hello, world!
+
+	char val_char;
+	int val_int;
+	long val_long;
+	float val_float;
+	double val_double;
+
+	//val_char = 'f';
+	//val_int = 10;
+	//val_long = 4000000000;
+	//val_float = 3.1415926589;
+	//val_double = 31415926589.123456789;
+
+
+	printf(" %8x\n %8x\n %8x\n %8x\n", &val_char, &val_int, &val_long, &val_double);
+	scanf("%c %d %d %f", &val_char, &val_int, &val_long, &val_float);
+	
+	printf("\n\n\n\n\n\nHello, world!\n\n\n\n\n\n");
+	printf("  정수 출력 테스트 : [%12d]\n", val_int);
+	printf("  실수 출력 테스트 : [%12.8f]\n", val_float);
+	printf("  실수 출력 테스트 : [%12e]\n", val_double);
+
+	printf(" 8진수 출력 테스트 : [%12o]\n", val_int);
+	printf("16진수 출력 테스트 : [%12x]\n", val_int);
+
+	printf("  문자 출력 테스트 : [%c]\n", val_char);
+	printf("  문자 연산 테스트 : [%c]\n", val_char + 1);
+}
--- test/test_24.03.12/test_24.03.12/test.c
+++ test/test_24.03.12/test_24.03.12/test.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
 
-void test();
+//void test();
+void test03();
+void Dump(char* p, int len);
+void Copy(char* p1, char* p2);
 
 void main() {
-	test();
+	test03();
 }
 
-void test() 
-{
-	char ca[] = "Hello"; // ca[0]:'H' ... ca[4]=:'o' ca[5]:\0
-	for (int i = 0; i < 10; i++) {
-		printf("ca[%d]: %c  (%02x)\n", i,  ca[i], ca[i]);
-	}
-	ca[2] -= 1;
-	ca[3] -= 1;
-	for (int i = 0; i < 10; i++) {
-		printf("ca[%d]: %c  (%02x)\n", i,  ca[i], ca[i]);
-	}
+//void test() 
+//{
+//	char ca[] = "Hello"; // ca[0]:'H' ... ca[4]=:'o' ca[5]:\0
+//	for (int i = 0; i < 6; i++) {
+//		printf("ca[%d]: %c  (%02x)  [%08x]\n", i,  ca[i], ca[i], ca+i);
+//	}
+//
+//	int ia[] = { 10,20,30,40,50 };
+//	for (int i = 0; i < 6; i++) {
+//		printf("ia[%d]: %d  (%08x)  [%08x]\n", i, ia[i], ia[i], ia + i);
+//	}
+//
+//	int ia2[3][2] = { 10,20,30,40,50,60 };
+//	for (int y = 0; y < 3; y++) {
+//		for (int x = 0; x < 2; x++) {
+//			printf("ia2[%d][%d]: %d  (%08x)\n", y, x, ia2[y][x], ia2[y][x]);
+//		}
+//	}
+//}
 
+void test03() {
+	char buf[100];		// 안전 메모리 공간
+	char* pbuf;			// 안전 메모리 공간중의 출력 위치
+	unsigned int addr;	// 출력 위치 지정을 위한 입력변수(주소)
+	char* kbuf[100];	// 출력 문자열 입력 공간
+
+	printf("Address of stable space is %d[%08x].\n", buf, buf);
+	printf("Enter the starting address : ");
+	scanf("%d", &addr);	// 안전 공간 주소 참고
+	pbuf = buf + addr;
+
+	printf("Enter the string : ");
+	scanf("%s", *kbuf);
+	Copy(*pbuf, *kbuf);
+	Dump(*buf, 100);
+}
+
+void Dump(char *p, int len)		// 메모리 공간 출력용 범용 함수
+{
+	for (int i = 0; i < len; i++)	// 안전 공간 메모리 덤프
+	{	
+		if (i % 16 == 0)	printf("\n%08x   ", p);
+		if (i % 8 == 0)	printf("  ");
+		printf("%02x ", (unsigned char)*p++);
+	}
+}
+
+void Copy(char *p1, char *p2)
+{
+	while (*p2) *p1++ = *p2++; *p1 = 0;
 }
